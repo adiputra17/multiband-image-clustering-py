020e5c5c99d62dc7368b4e099bd0c8e3ed7a5017
adiputra17 <adiputrautama17@gmail.com>
2018-05-28 17:06:03 +0100

    + hitung jarak

--- app.py
+++ app.py
@@ -16,73 +16,59 @@ app = Flask(__name__, static_url_path = "/images", static_folder = "images")
 # g.save('images/1.jpg')
 # g.show()
 
-im1             = Image.open("images/gb1.GIF")
-im2             = Image.open("images/gb2.GIF")
-im3             = Image.open("images/gb3.GIF")
-im4             = Image.open("images/gb4.GIF")
-im5             = Image.open("images/gb5.GIF")
-im7             = Image.open("images/gb7.GIF")
-width, height   = im1.size
-rgb_im1         = im1.convert('RGB')
-rgb_im2         = im2.convert('RGB')
-rgb_im3         = im3.convert('RGB')
-rgb_im4         = im4.convert('RGB')
-rgb_im5         = im5.convert('RGB')
-rgb_im7         = im7.convert('RGB')
+# im1             = Image.open("images/gb1.GIF")
+# im2             = Image.open("images/gb2.GIF")
+# im3             = Image.open("images/gb3.GIF")
+# im4             = Image.open("images/gb4.GIF")
+# im5             = Image.open("images/gb5.GIF")
+# im7             = Image.open("images/gb7.GIF")
+# width, height   = im1.size
+# rgb_im2         = im2.convert('RGB')
+# rgb_im3         = im3.convert('RGB')
+# rgb_im4         = im4.convert('RGB')
+# rgb_im5         = im5.convert('RGB')
+# rgb_im7         = im7.convert('RGB')
 
-list_gray = {}
+w, h = 6, 1024;
+list_data = [[0 for x in range(w)] for y in range(h)] 
 x = 0
+T = 0
+sum_data = 0
+isLooping = [[0 for a in range(w)] for b in range(h)] 
+rgb_im = {}
+im = {}
+# distance = [[0 for c in range(10)] for d in range(10)]
+distance = [[0 for c in range(1024)] for d in range(1024)]
+list_tmp = []
+
+# get image
+for k in xrange(0,6):
+    if k == 5:
+        url = 'images/gb'+str(k+2)+'.GIF'
+    else :
+        url = 'images/gb'+str(k+1)+'.GIF'
+       
+    im[k]           = Image.open(url)
+    rgb_im[k]       = im[k].convert('RGB')
+    width, height   = im[k].size
+
+# get pixel then enter to array
 for i in xrange(0,width):
     for j in xrange(0,height):
-        # image1
-        pixel_im1   = rgb_im1.getpixel((i,j))
-        red_im1     = pixel_im1[0]
-        green_im1   = pixel_im1[1]
-        blue_im1    = pixel_im1[2]
-        gray_im1    = (red_im1 + green_im1 + blue_im1)/3
+        for k in xrange(0,6):
 
-        # image2
-        pixel_im2   = rgb_im2.getpixel((i,j))
-        red_im2     = pixel_im2[0]
-        green_im2   = pixel_im2[1]
-        blue_im2    = pixel_im2[2]
-        gray_im2    = (red_im2 + green_im2 + blue_im2)/3
+            pixel_im   = rgb_im[k].getpixel((i,j))
+            red_im     = pixel_im[0]
+            green_im   = pixel_im[1]
+            blue_im    = pixel_im[2]
+            gray_im    = (red_im + green_im + blue_im)/3
 
-        # image3
-        pixel_im3   = rgb_im3.getpixel((i,j))
-        red_im3     = pixel_im3[0]
-        green_im3   = pixel_im3[1]
-        blue_im3    = pixel_im3[2]
-        gray_im3    = (red_im3 + green_im3 + blue_im3)/3
+            list_data[x][k] = gray_im
 
-        # image4
-        pixel_im4   = rgb_im4.getpixel((i,j))
-        red_im4     = pixel_im4[0]
-        green_im4   = pixel_im4[1]
-        blue_im4    = pixel_im4[2]
-        gray_im4    = (red_im4 + green_im4 + blue_im4)/3
-
-        # image5
-        pixel_im5   = rgb_im5.getpixel((i,j))
-        red_im5     = pixel_im5[0]
-        green_im5   = pixel_im5[1]
-        blue_im5    = pixel_im5[2]
-        gray_im5    = (red_im5 + green_im5 + blue_im5)/3
-
-        # image7
-        pixel_im7   = rgb_im7.getpixel((i,j))
-        red_im7     = pixel_im7[0]
-        green_im7   = pixel_im7[1]
-        blue_im7    = pixel_im7[2]
-        gray_im7    = (red_im7 + green_im7 + blue_im7)/3
-
-        # list_gray[i][j] = [str(gray_im1), str(gray_im2), str(gray_im3), str(gray_im4), str(gray_im5), str(gray_im7)]
-        list_gray[x] = [gray_im1, gray_im2, gray_im3, gray_im4, gray_im5, gray_im7]
+            isLooping[x][k] = True
 
         x = x + 1
 
-        # list_gray.append(str(gray_im1), str(gray_im2), str(gray_im3), str(gray_im4), str(gray_im5), str(gray_im7))
-        # print str(gray_im1), str(gray_im2), str(gray_im3), str(gray_im4), str(gray_im5), str(gray_im7)
 
 @app.route('/')
 def main():
@@ -94,8 +80,24 @@ def process():
         return('<form action="/test" method="post"><input type="submit" value="Send" /></form>')
 
     elif request.method =='POST':
-        cluster = request.form['cluster']
-        return "jumlah cluster : " + str(cluster) + '\n' + str(list_gray[7])
+        cluster  = request.form['cluster']
+        sum_data = len(list_data)
+        x = 0
+
+        # while sum_data > 3:
+        for i in xrange(0,1024): #baris 1
+            for j in xrange(0,1024): #baris 2
+                for k in xrange(0,6): #kolom
+                    # if isLooping[x][k] == True:
+                        T = list_data[i][k] - list_data[j][k]
+                        list_tmp.append(abs(T))
+
+                print list_tmp
+                distance[i][j] = np.array(list_tmp)
+                list_tmp[:] = []
+                x = x + 1
+
+        return "jumlah cluster : " + str(cluster) + '\n' + str(distance)
 
     else:
         return "ok"
